e2b0b5a123dd102b8db78ceb4577ecd42417046c
Muratcan Erdoğan <erdoganmcan@gmail.com>
2022-03-05 00:27:09 +0300

    Navbar and Footer component created and added to Layout

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import { Box } from "@chakra-ui/react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 const Layout = ({ children }) => (
   <>
@@ -7,9 +9,13 @@ const Layout = ({ children }) => (
       <title>Real Estate</title>
     </Head>
     <Box maxWidth="1280px" m="auto">
-      <header>Navbar</header>
+      <header>
+        <Navbar />
+      </header>
       <main>{children}</main>
-      <footer>Footer</footer>
+      <footer>
+        <Footer />
+      </footer>
     </Box>
   </>
 );
